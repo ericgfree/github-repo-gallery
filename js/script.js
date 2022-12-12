cebb38cbb4680b60406724477a172521ade2155a
ericgfree <ericgfreeman@gmail.com>
2022-12-12 15:36:31 -0500

    List number of repos and repo names

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 const overviewElement = document.querySelector(".overview")
 // My username
 const username = "ericgfree"
+// Unordered list for repos
+const displayReposElement = document.querySelector(".repo-list")
+
+
+
 //  My profile information
 const myUserInfo = async function () {
     const userInfo = await fetch(
@@ -28,4 +33,25 @@ const showUserInfo = function (data) {
         <p><strong>Number of public repos:</strong>${data.public_repos}</p>
     </div>`;
     overviewElement.append(div);
+    myUserRepos();
+};
+
+
+// Fetch repos
+const myUserRepos = async function () {
+    const userRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=10`);
+    const repoData = await userRepos.json();
+    // console.log(myUserRepos)
+    showRepoData(repoData)
+};
+
+
+// Display repo information
+const showRepoData = function (repos) {
+    for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    displayReposElement.append(repoItem);
+    }
 };
